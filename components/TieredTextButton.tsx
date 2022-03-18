e6c3279d33cc128470c80579f9ce9c0a6b34ea7f
McCoy Zhu <mz1945@nyu.edu>
2022-03-18 17:22:59 -0400

    ADD: `TieredTextButton`

---
+++ components/TieredTextButton.tsx
@@ -0,0 +1,34 @@
+import { Pressable, VStack, Text, type IPressableProps } from "native-base";
+
+type TieredTextButtonProps = IPressableProps & {
+  primaryText: string;
+  secondaryText?: string;
+};
+
+export default function TieredTextButton({
+  primaryText,
+  secondaryText,
+  ...rest
+}: TieredTextButtonProps) {
+  return (
+    <Pressable {...rest} background={"background.secondary"} borderRadius={12}>
+      <VStack
+        height={"100%"}
+        justifyContent={"center"}
+        marginX={2}
+        textAlign={"center"}
+      >
+        <Text
+          fontSize={"md"}
+          fontWeight={"medium"}
+          lineHeight={"sm"}
+          textAlign={"center"}
+          numberOfLines={2}
+        >
+          {primaryText}
+        </Text>
+        {secondaryText && <Text textAlign={"center"}>{secondaryText}</Text>}
+      </VStack>
+    </Pressable>
+  );
+}

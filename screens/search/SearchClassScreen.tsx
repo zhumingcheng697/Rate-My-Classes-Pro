3021aef280588edde6a40e73cfba5d0abf4ff944
McCoy Zhu <mz1945@nyu.edu>
2022-03-19 01:02:14 -0400

    ADD: `onChangeText`, keybord dismiss, and prompt in `SearchClassScreen`

--- screens/search/SearchClassScreen.tsx
+++ screens/search/SearchClassScreen.tsx
@@ -1,4 +1,14 @@
-import { Input, Icon, ScrollView } from "native-base";
+import { useState } from "react";
+import {
+  Input,
+  Icon,
+  ScrollView,
+  Text,
+  VStack,
+  Spacer,
+  Pressable,
+} from "native-base";
+import { Keyboard } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { type StackNavigationProp } from "@react-navigation/stack";
 import Ionicons from "react-native-vector-icons/Ionicons";
@@ -16,52 +26,92 @@ type SearchClassScreenNavigationProp = StackNavigationProp<
 
 export default function SearchClassScreen() {
   const navigation = useNavigation<SearchClassScreenNavigationProp>();
+  const [query, setQuery] = useState("");
 
   return (
     <SafeAreaView edges={["left", "right"]}>
-      <Input
-        size={"lg"}
-        margin={"10px"}
-        borderWidth={1.5}
-        placeholder={"Search Class Name or Description"}
-        _focus={{ borderColor: "nyu.default" }}
-        leftElement={
-          <Icon
-            marginLeft={"5px"}
-            marginRight={"-5px"}
-            size={"sm"}
-            color={"gray.400"}
-            as={<Ionicons name={"search"} />}
-          />
-        }
-      ></Input>
-      <ScrollView>
-        <Grid minChildrenWidth={140} childrenHeight={"90px"}>
-          {["2193", "3193", "4193"].map((classNumber, index) => {
-            const classCode: ClassCode = {
-              schoolCode: "UY",
-              departmentCode: "DM",
-              classNumber,
-            };
-
-            return (
-              <TieredTextButton
-                key={index}
-                primaryText={"Lorem ipsum dolor sit amet"}
-                secondaryText={getClassCode(classCode)}
-                onPress={() => {
-                  navigation.navigate("Search-Detail", {
-                    ...classCode,
-                    name: classNumber,
-                    description:
-                      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas modi explicabo fuga, eum libero ipsum magnam. Dolores, vel vero nobis doloribus voluptatibus soluta ratione adipisci repellat voluptatem libero ipsam rerum.",
-                  });
-                }}
+      <Pressable
+        onPress={() => {
+          Keyboard.dismiss();
+        }}
+      >
+        <VStack height={"100%"}>
+          <Input
+            size={"lg"}
+            margin={"10px"}
+            borderWidth={1.5}
+            placeholder={"Search"}
+            _focus={{ borderColor: "nyu.default" }}
+            value={query}
+            onChangeText={setQuery}
+            leftElement={
+              <Icon
+                marginLeft={"5px"}
+                marginRight={"-5px"}
+                size={"sm"}
+                color={"gray.400"}
+                as={<Ionicons name={"search"} />}
               />
-            );
-          })}
-        </Grid>
-      </ScrollView>
+            }
+            rightElement={
+              query ? (
+                <Pressable
+                  onPress={() => {
+                    setQuery("");
+                  }}
+                >
+                  {({ isPressed, isHovered }) => (
+                    <Icon
+                      marginLeft={"-5px"}
+                      marginRight={"5px"}
+                      size={"sm"}
+                      color={isPressed || isHovered ? "gray.500" : "gray.400"}
+                      as={<Ionicons name={"close-circle"} />}
+                    />
+                  )}
+                </Pressable>
+              ) : undefined
+            }
+          ></Input>
+          {query ? (
+            <ScrollView>
+              <Grid minChildrenWidth={140} childrenHeight={"90px"}>
+                {["2193", "3193", "4193"].map((classNumber, index) => {
+                  const classCode: ClassCode = {
+                    schoolCode: "UY",
+                    departmentCode: "DM",
+                    classNumber,
+                  };
+
+                  return (
+                    <TieredTextButton
+                      key={index}
+                      primaryText={"Lorem ipsum dolor sit amet"}
+                      secondaryText={getClassCode(classCode)}
+                      onPress={() => {
+                        navigation.navigate("Search-Detail", {
+                          ...classCode,
+                          name: classNumber,
+                          description:
+                            "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas modi explicabo fuga, eum libero ipsum magnam. Dolores, vel vero nobis doloribus voluptatibus soluta ratione adipisci repellat voluptatem libero ipsam rerum.",
+                        });
+                      }}
+                    />
+                  );
+                })}
+              </Grid>
+            </ScrollView>
+          ) : (
+            <>
+              <Spacer key={"spacer-top"} />
+              <Text key={"text-center"} textAlign={"center"}>
+                Search Classes by Title or Description
+              </Text>
+              <Spacer key={"spacer-down"} />
+            </>
+          )}
+        </VStack>
+      </Pressable>
     </SafeAreaView>
   );
 }

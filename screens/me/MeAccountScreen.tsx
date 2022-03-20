da5b9d586a9452bab010b721fc3064dc597160fe
McCoy Zhu <mz1945@nyu.edu>
2022-03-20 02:18:44 -0400

    ADD: sign out `AlertDialog`

--- screens/me/MeAccountScreen.tsx
+++ screens/me/MeAccountScreen.tsx
@@ -1,4 +1,5 @@
-import { Text, VStack } from "native-base";
+import { useState, useRef } from "react";
+import { Text, VStack, AlertDialog, Button } from "native-base";
 import { useNavigation } from "@react-navigation/native";
 import { type StackNavigationProp } from "@react-navigation/stack";
 
@@ -13,39 +14,83 @@ type MeAccountScreenNavigationProp = StackNavigationProp<
 
 export default function MeAccountScreen() {
   const navigation = useNavigation<MeAccountScreenNavigationProp>();
+  const ref = useRef();
+  const [showAlert, setShowAlert] = useState(false);
 
   return (
-    <KeyboardAwareSafeAreaScrollView>
-      <Text variant={"h1"}>McCoy Applseed</Text>
-      <VStack margin={"10px"} space={"12px"}>
-        <LeftAlignedButton
-          title={"Starred"}
-          onPress={() => {
-            navigation.push("Me-Starred");
-          }}
-        />
-        <LeftAlignedButton
-          title={"Reviewed"}
-          onPress={() => {
-            navigation.push("Me-Reviewed");
-          }}
-        />
-        <LeftAlignedButton
-          title={"Settings"}
-          onPress={() => {
-            navigation.push("Me-Settings");
-          }}
-        />
-        <LeftAlignedButton
-          title={"Sign Out"}
-          _text={{ color: "red.600" }}
-          showChevron={false}
-          marginTop={"15px"}
-          onPress={() => {
-            navigation.replace("Me-SignIn");
-          }}
-        />
-      </VStack>
-    </KeyboardAwareSafeAreaScrollView>
+    <>
+      <AlertDialog
+        leastDestructiveRef={ref}
+        isOpen={showAlert}
+        onClose={() => {
+          setShowAlert(false);
+        }}
+      >
+        <AlertDialog.Content>
+          <AlertDialog.Header>Sign Out</AlertDialog.Header>
+          <AlertDialog.Body>
+            You are about to sign out. After you signed out, you will have to
+            sign in again with your email and password.
+          </AlertDialog.Body>
+          <AlertDialog.Footer>
+            <Button.Group space={2}>
+              <Button
+                variant="unstyled"
+                colorScheme="coolGray"
+                _pressed={{ opacity: 0.5 }}
+                _hover={{ opacity: 0.5 }}
+                onPress={() => {
+                  setShowAlert(false);
+                }}
+                ref={ref}
+              >
+                Cancel
+              </Button>
+              <Button
+                background={"red.600"}
+                onPress={() => {
+                  setShowAlert(false);
+                  navigation.replace("Me-SignIn");
+                }}
+              >
+                Sign Out
+              </Button>
+            </Button.Group>
+          </AlertDialog.Footer>
+        </AlertDialog.Content>
+      </AlertDialog>
+      <KeyboardAwareSafeAreaScrollView>
+        <Text variant={"h1"}>McCoy Applseed</Text>
+        <VStack margin={"10px"} space={"12px"}>
+          <LeftAlignedButton
+            title={"Starred"}
+            onPress={() => {
+              navigation.push("Me-Starred");
+            }}
+          />
+          <LeftAlignedButton
+            title={"Reviewed"}
+            onPress={() => {
+              navigation.push("Me-Reviewed");
+            }}
+          />
+          <LeftAlignedButton
+            title={"Settings"}
+            onPress={() => {
+              navigation.push("Me-Settings");
+            }}
+          />
+          <LeftAlignedButton
+            title={"Sign Out"}
+            _text={{ color: "red.600" }}
+            showChevron={false}
+            marginTop={"15px"}
+            onPress={() => {
+              setShowAlert(true);
+            }}
+          />
+        </VStack>
+      </KeyboardAwareSafeAreaScrollView>
+    </>
   );
 }

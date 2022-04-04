7994e80df5b27b54d5e734d4bdba44044778eea6
McCoy Zhu <mz1945@nyu.edu>
2022-04-04 11:46:44 -0400

    UPDATE: `StarredReviewedScreen` showing starred classes with `parseClassCode`

--- screens/me/StarredReviewedScreen.tsx
+++ screens/me/StarredReviewedScreen.tsx
@@ -1,16 +1,21 @@
 import React from "react";
-import { Text, Box } from "native-base";
+import { Box } from "native-base";
 import {
   useNavigation,
   useRoute,
   type RouteProp,
 } from "@react-navigation/native";
 import { type StackNavigationProp } from "@react-navigation/stack";
+import { useSelector } from "react-redux";
 
 import type { MeNavigationParamList, ClassCode } from "../../shared/types";
 import KeyboardAwareSafeAreaScrollView from "../../containers/KeyboardAwareSafeAreaScrollView";
 import Grid from "../../containers/Grid";
-import { getFullClassCode, placeholderClassNumbers } from "../../shared/utils";
+import {
+  getFullClassCode,
+  parseClassCode,
+  placeholderClassNumbers,
+} from "../../shared/utils";
 import TieredTextButton from "../../components/TieredTextButton";
 
 type StarredReviewedScreenNavigationProp = StackNavigationProp<
@@ -24,39 +29,40 @@ type StarredReviewedScreenRouteProp = RouteProp<
 >;
 
 export default function StarredReviewedScreen() {
+  const starredClasses = useSelector((state) => state.starredClassRecord);
   const navigation = useNavigation<StarredReviewedScreenNavigationProp>();
   const route = useRoute<StarredReviewedScreenRouteProp>();
 
   return (
     <KeyboardAwareSafeAreaScrollView>
       <Box marginY={"10px"}>
-        {/* <Text variant={"h1"}>{route.name.replace(/Me-/gi, "")}</Text> */}
         <Grid>
           {(info) =>
-            placeholderClassNumbers.map((classNumber, index) => {
-              const classCode: ClassCode = {
-                schoolCode: "UY",
-                departmentCode: "DM",
-                classNumber,
-              };
-
-              return (
-                <TieredTextButton
-                  key={index}
-                  {...info}
-                  primaryText={"Lorem ipsum dolor sit amet"}
-                  secondaryText={getFullClassCode(classCode)}
-                  onPress={() => {
-                    navigation.navigate("Detail", {
-                      ...classCode,
-                      name: classNumber,
-                      description:
-                        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas modi explicabo fuga, eum libero ipsum magnam. Dolores, vel vero nobis doloribus voluptatibus soluta ratione adipisci repellat voluptatem libero ipsam rerum.",
-                    });
-                  }}
-                />
-              );
-            })
+            (route.name === "Starred"
+              ? (Object.keys(starredClasses)
+                  .map(parseClassCode)
+                  .filter((e) => e != null) as ClassCode[])
+              : placeholderClassNumbers.map((classNumber) => ({
+                  schoolCode: "UY",
+                  departmentCode: "DM",
+                  classNumber,
+                }))
+            ).map((classCode, index) => (
+              <TieredTextButton
+                key={index}
+                {...info}
+                primaryText={"Lorem ipsum dolor sit amet"}
+                secondaryText={getFullClassCode(classCode)}
+                onPress={() => {
+                  navigation.navigate("Detail", {
+                    ...classCode,
+                    name: classCode.classNumber,
+                    description:
+                      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas modi explicabo fuga, eum libero ipsum magnam. Dolores, vel vero nobis doloribus voluptatibus soluta ratione adipisci repellat voluptatem libero ipsam rerum.",
+                  });
+                }}
+              />
+            ))
           }
         </Grid>
       </Box>

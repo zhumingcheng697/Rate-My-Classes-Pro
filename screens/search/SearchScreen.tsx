e21e42ed1698a1902ef499d77bf291b0d2c7b375
McCoy Zhu <mz1945@nyu.edu>
2022-04-06 22:08:13 -0400

    UPDATE: `SearchScreen` actually searching

--- screens/search/SearchScreen.tsx
+++ screens/search/SearchScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, useEffect } from "react";
 import { useWindowDimensions } from "react-native";
 import { useSelector } from "react-redux";
 import { Text, Center, Divider, Box } from "native-base";
@@ -8,8 +8,9 @@ import { useNavigation } from "@react-navigation/native";
 import { type StackNavigationProp } from "@react-navigation/stack";
 
 import { inputSelectHeight } from "../../shared/theme";
-import type { SearchNavigationParamList, ClassCode } from "../../shared/types";
-import { getFullClassCode, placeholderClassNumbers } from "../../shared/utils";
+import type { SearchNavigationParamList, ClassInfo } from "../../shared/types";
+import { getFullClassCode } from "../../shared/utils";
+import { searchClasses } from "../../shared/schedge";
 import KeyboardAwareSafeAreaScrollView from "../../containers/KeyboardAwareSafeAreaScrollView";
 import Grid from "../../containers/Grid";
 import SearchBar from "../../components/SearchBar";
@@ -22,16 +23,59 @@ type SearchScreenNavigationProp = StackNavigationProp<
 
 const dividerHeight = 1;
 const searchBarMargin = 10;
+const delay = 500; // only make API call if idle for at least this amount of time
 
 export default function SearchScreen() {
   const navigation = useNavigation<SearchScreenNavigationProp>();
   const [query, setQuery] = useState("");
   const [focused, setFocused] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [matchedClasses, setMatchedClass] = useState<ClassInfo[]>([]);
   const { selectedSemester } = useSelector((state) => state.settings);
 
   const { height } = useWindowDimensions();
   const headerHeight = useHeaderHeight();
   const tabBarHeight = useBottomTabBarHeight();
+
+  const search = useCallback(
+    (() => {
+      let timeoutId: ReturnType<typeof setTimeout>;
+      let shouldDiscard = true;
+
+      return (query: string, selectedSemester) => {
+        clearTimeout(timeoutId);
+        setMatchedClass([]);
+        shouldDiscard = true;
+
+        if (query) {
+          setIsLoaded(false);
+          timeoutId = setTimeout(() => {
+            shouldDiscard = false;
+
+            searchClasses(query, selectedSemester)
+              .then((matches) => {
+                if (!shouldDiscard) {
+                  setMatchedClass(matches);
+                  setIsLoaded(true);
+                } else {
+                  setMatchedClass([]);
+                }
+              })
+              .catch((e) => {
+                console.error(e);
+              });
+          }, delay);
+        } else {
+          setIsLoaded(true);
+        }
+      };
+    })(),
+    []
+  );
+
+  useEffect(() => {
+    search(query, selectedSemester);
+  }, [query, selectedSemester]);
 
   return (
     <KeyboardAwareSafeAreaScrollView
@@ -55,29 +99,18 @@ export default function SearchScreen() {
         />
         <Divider height={`${dividerHeight}px`} />
       </Box>
-      {query ? (
-        <Grid marginY={"10px"}>
+      {focused || query ? (
+        <Grid marginY={"10px"} isLoaded={isLoaded}>
           {(info) =>
-            placeholderClassNumbers.map((classNumber, index) => {
-              const classCode: ClassCode = {
-                schoolCode: "UY",
-                departmentCode: "DM",
-                classNumber,
-              };
-
+            matchedClasses.map((classInfo, index) => {
               return (
                 <TieredTextButton
                   key={index}
                   {...info}
-                  primaryText={"Lorem ipsum dolor sit amet"}
-                  secondaryText={getFullClassCode(classCode)}
+                  primaryText={classInfo.name}
+                  secondaryText={getFullClassCode(classInfo)}
                   onPress={() => {
-                    navigation.navigate("Detail", {
-                      ...classCode,
-                      name: classNumber,
-                      description:
-                        "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas modi explicabo fuga, eum libero ipsum magnam. Dolores, vel vero nobis doloribus voluptatibus soluta ratione adipisci repellat voluptatem libero ipsam rerum.",
-                    });
+                    navigation.navigate("Detail", classInfo);
                   }}
                 />
               );
@@ -85,25 +118,23 @@ export default function SearchScreen() {
           }
         </Grid>
       ) : (
-        !focused && (
-          <Center
-            height={`${
-              height -
-              headerHeight -
-              tabBarHeight -
-              inputSelectHeight -
-              dividerHeight -
-              searchBarMargin * 2
-            }px`}
-          >
-            <Text textAlign={"center"} fontSize={"md"}>
-              Search {selectedSemester.toString()} Classes
-            </Text>
-            <Text textAlign={"center"} fontSize={"sm"}>
-              (By Title or Description)
-            </Text>
-          </Center>
-        )
+        <Center
+          height={`${
+            height -
+            headerHeight -
+            tabBarHeight -
+            inputSelectHeight -
+            dividerHeight -
+            searchBarMargin * 2
+          }px`}
+        >
+          <Text textAlign={"center"} fontSize={"md"}>
+            Search {selectedSemester.toString()} Classes
+          </Text>
+          <Text textAlign={"center"} fontSize={"sm"}>
+            (By Title or Description)
+          </Text>
+        </Center>
       )}
     </KeyboardAwareSafeAreaScrollView>
   );

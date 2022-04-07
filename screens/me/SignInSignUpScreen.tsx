e1a5f85446450f5ee804fc7bd41f48c55abd34e1
McCoy Zhu <mz1945@nyu.edu>
2022-04-07 14:26:38 -0400

    UPDATE: `SignInSignUpScreen` actually functional

--- screens/me/SignInSignUpScreen.tsx
+++ screens/me/SignInSignUpScreen.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { Text, Button, Input, VStack, Box } from "native-base";
-import { useNavigation } from "@react-navigation/native";
+import {
+  useNavigation,
+  useRoute,
+  type RouteProp,
+} from "@react-navigation/native";
 import { type StackNavigationProp } from "@react-navigation/stack";
 
 import { type MeNavigationParamList } from "../../shared/types";
@@ -12,16 +16,26 @@ type SignInSignUpScreenNavigationProp = StackNavigationProp<
   "SignInSignUp"
 >;
 
+type SignInSignUpScreenRouteProp = RouteProp<
+  MeNavigationParamList,
+  "SignInSignUp"
+>;
+
 export default function SignInSignUpScreen() {
   const navigation = useNavigation<SignInSignUpScreenNavigationProp>();
+  const route = useRoute<SignInSignUpScreenRouteProp>();
+
+  const isSigningIn = route.params.isSigningIn;
 
   return (
     <KeyboardAwareSafeAreaScrollView>
       <VStack margin={"10px"} space={"8px"}>
-        <Box>
-          <Text variant={"label"}>Username</Text>
-          <Input autoCompleteType={"username"} />
-        </Box>
+        {!isSigningIn && (
+          <Box>
+            <Text variant={"label"}>Username</Text>
+            <Input autoCompleteType={"username"} />
+          </Box>
+        )}
         <Box>
           <Text variant={"label"}>Email</Text>
           <Input
@@ -35,27 +49,52 @@ export default function SignInSignUpScreen() {
           <Text variant={"label"}>Password</Text>
           <Input variant={"password"} />
         </Box>
-        <Box>
-          <Text variant={"label"}>Confirm Password</Text>
-          <Input variant={"password"} />
-        </Box>
-        <Button
-          marginY={"15px"}
-          onPress={() => {
-            navigation.navigate("Account", { isSignedIn: true });
-          }}
-        >
-          <Text variant={"button"}>Sign Up</Text>
-        </Button>
-        <Box>
-          <Text textAlign={"center"}>Already have an account?</Text>
-          <PlainTextButton
-            title={"Sign In"}
+        {!isSigningIn && (
+          <Box>
+            <Text variant={"label"}>Confirm Password</Text>
+            <Input variant={"password"} />
+          </Box>
+        )}
+        {isSigningIn ? (
+          <Button
+            marginY={"15px"}
             onPress={() => {
-              navigation.navigate("SignInSignUp", { isSigningIn: true });
+              navigation.navigate("Account", { isSignedIn: true });
             }}
-          />
-        </Box>
+          >
+            <Text variant={"button"}>Sign In</Text>
+          </Button>
+        ) : (
+          <Button
+            marginY={"15px"}
+            onPress={() => {
+              navigation.navigate("Account", { isSignedIn: true });
+            }}
+          >
+            <Text variant={"button"}>Sign Up</Text>
+          </Button>
+        )}
+        {isSigningIn ? (
+          <Box>
+            <Text textAlign={"center"}>Don’t have an account yet?</Text>
+            <PlainTextButton
+              title={"Sign Up"}
+              onPress={() => {
+                navigation.navigate("SignInSignUp", { isSigningIn: false });
+              }}
+            />
+          </Box>
+        ) : (
+          <Box>
+            <Text textAlign={"center"}>Already have an account?</Text>
+            <PlainTextButton
+              title={"Sign In"}
+              onPress={() => {
+                navigation.navigate("SignInSignUp", { isSigningIn: true });
+              }}
+            />
+          </Box>
+        )}
       </VStack>
     </KeyboardAwareSafeAreaScrollView>
   );

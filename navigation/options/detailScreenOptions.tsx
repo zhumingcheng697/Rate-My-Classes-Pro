8270060b02a5dbee59d6dfed9b0728a55e6866ec
McCoy Zhu <mz1945@nyu.edu>
2022-04-10 18:56:31 -0400

    ADD: "Sign Up to Star" `AlertPopup`

--- navigation/options/detailScreenOptions.tsx
+++ navigation/options/detailScreenOptions.tsx
@@ -1,52 +1,108 @@
-import React from "react";
-import { IconButton, Icon } from "native-base";
-import { type RouteProp } from "@react-navigation/native";
-import { type StackNavigationOptions } from "@react-navigation/stack";
+import React, { useState } from "react";
+import { IconButton, Icon, Button } from "native-base";
+import { useIsFocused, type RouteProp } from "@react-navigation/native";
+import type {
+  StackNavigationProp,
+  StackNavigationOptions,
+} from "@react-navigation/stack";
 import { useSelector, useDispatch } from "react-redux";
 import Ionicons from "react-native-vector-icons/Ionicons";
 
 import { starClass, unstarClass } from "../../redux/actions";
 import { type SharedNavigationParamList } from "../../libs/types";
 import { getFullClassCode } from "../../libs/utils";
+import { useAuth } from "../../mongodb/auth";
+import AlertPopup from "../../components/AlertPopup";
+
+type DetailScreenNavigationProp = StackNavigationProp<
+  SharedNavigationParamList,
+  "Detail"
+>;
 
 type DetailScreenRouteProp = RouteProp<SharedNavigationParamList, "Detail">;
 
-export type DetailScreenOptionsProp = { route: DetailScreenRouteProp };
+export type DetailScreenOptionsProp = {
+  navigation: DetailScreenNavigationProp;
+  route: DetailScreenRouteProp;
+};
 
 export default ({
+  navigation,
   route,
 }: DetailScreenOptionsProp): StackNavigationOptions => ({
   title: getFullClassCode(route.params),
   headerRight: (props) => {
     const starredClasses = useSelector((state) => state.starredClassRecord);
     const dispatch = useDispatch();
+    const auth = useAuth();
+    const isFocused = useIsFocused();
+    const [showAlert, setShowAlert] = useState(false);
     const isStarred =
-      starredClasses && !!starredClasses[getFullClassCode(route.params)];
+      auth.isAuthenticated &&
+      starredClasses &&
+      !!starredClasses[getFullClassCode(route.params)];
     const pressedHoverStyle = { _icon: { opacity: 0.5 } };
 
     return (
-      <IconButton
-        variant={"unstyled"}
-        marginRight={"5px"}
-        padding={"5px"}
-        _pressed={pressedHoverStyle}
-        _hover={pressedHoverStyle}
-        icon={
-          <Icon
-            color={isStarred ? "yellow.400" : "gray.300"}
-            size={"22px"}
-            as={<Ionicons name={"star" + (isStarred ? "" : "-outline")} />}
-          />
-        }
-        onPress={() => {
-          if (isStarred) {
-            unstarClass(dispatch)(route.params);
-          } else {
-            starClass(dispatch)(route.params);
+      <>
+        <AlertPopup
+          isOpen={showAlert && isFocused}
+          onClose={() => {
+            setShowAlert(false);
+          }}
+          header={"Sign Up to Star"}
+          body={"You need an account to keep track of your starred classes."}
+          footer={(ref) => (
+            <Button.Group space={2}>
+              <Button
+                variant="unstyled"
+                _pressed={{ opacity: 0.5 }}
+                _hover={{ opacity: 0.5 }}
+                onPress={() => {
+                  setShowAlert(false);
+                }}
+                ref={ref}
+              >
+                Cancel
+              </Button>
+              <Button
+                onPress={() => {
+                  setShowAlert(false);
+                  navigation.navigate("SignInSignUp");
+                }}
+              >
+                Sign Up
+              </Button>
+            </Button.Group>
+          )}
+        />
+        <IconButton
+          variant={"unstyled"}
+          marginRight={"5px"}
+          padding={"5px"}
+          _pressed={pressedHoverStyle}
+          _hover={pressedHoverStyle}
+          icon={
+            <Icon
+              color={isStarred ? "yellow.400" : "gray.300"}
+              size={"22px"}
+              as={<Ionicons name={"star" + (isStarred ? "" : "-outline")} />}
+            />
           }
-        }}
-        {...props}
-      />
+          onPress={() => {
+            if (auth.isAuthenticated) {
+              if (isStarred) {
+                unstarClass(dispatch)(route.params);
+              } else {
+                starClass(dispatch)(route.params);
+              }
+            } else {
+              setShowAlert(true);
+            }
+          }}
+          {...props}
+        />
+      </>
     );
   },
 });

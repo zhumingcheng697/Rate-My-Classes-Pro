b6f9477e19b45a14173c806688842d23b4a997ac
McCoy Zhu <mz1945@nyu.edu>
2022-04-11 18:31:44 -0400

    ADD: update username functionality on `SettingsScreen`

--- screens/me/SettingsScreen.tsx
+++ screens/me/SettingsScreen.tsx
@@ -1,16 +1,22 @@
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Select, Icon, Box, Text, HStack, Switch, VStack } from "native-base";
 import Ionicons from "react-native-vector-icons/Ionicons";
 
 import Semester from "../../libs/semester";
+import ClearableInput from "../../components/ClearableInput";
 import KeyboardAwareSafeAreaScrollView from "../../containers/KeyboardAwareSafeAreaScrollView";
 import { selectSemester, setShowPreviousSemesters } from "../../redux/actions";
+import { useAuth } from "../../mongodb/auth";
 
 type SemesterOptionRecord = Record<string, Semester>;
 
 export default function SettingsScreen() {
+  const auth = useAuth();
   const dispatch = useDispatch();
+  const [canClear, setCanClear] = useState(false);
+  const [previousUsername, setPreviousUsername] = useState(auth.username);
+  const [newUsername, setNewUsername] = useState(auth.username ?? "");
   const { selectedSemester, showPreviousSemesters } = useSelector(
     (state) => state.settings
   );
@@ -26,7 +32,36 @@ export default function SettingsScreen() {
 
   return (
     <KeyboardAwareSafeAreaScrollView>
-      <VStack margin={"10px"} space={"20px"}>
+      <VStack margin={"10px"} space={"8px"}>
+        {auth.isAuthenticated && (
+          <Box>
+            <Text variant={"label"} fontWeight={"semibold"} color={"nyu"}>
+              Username
+            </Text>
+            <ClearableInput
+              canClear={canClear && !!newUsername}
+              placeholder={previousUsername ?? "McCoy Appleseed"}
+              value={newUsername}
+              onChangeText={setNewUsername}
+              returnKeyType={"done"}
+              onFocus={() => {
+                setCanClear(true);
+              }}
+              onEndEditing={() => {
+                setCanClear(false);
+                if (newUsername) {
+                  setPreviousUsername(newUsername);
+                  if (newUsername !== auth.username)
+                    auth.updateUsername(newUsername);
+                } else if (!newUsername && previousUsername) {
+                  setNewUsername(previousUsername);
+                  if (previousUsername !== auth.username)
+                    auth.updateUsername(previousUsername);
+                }
+              }}
+            />
+          </Box>
+        )}
         <Box>
           <Text variant={"label"} fontWeight={"semibold"} color={"nyu"}>
             Semester
@@ -55,6 +90,7 @@ export default function SettingsScreen() {
           justifyContent={"space-between"}
           alignContent={"center"}
           alignItems={"center"}
+          marginY={"10px"}
         >
           <Text fontSize={"17px"} fontWeight={"semibold"} color={"nyu"}>
             Show Previous Semesters

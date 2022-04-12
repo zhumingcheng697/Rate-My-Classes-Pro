e334547887bc569d38484085cf17f2bd693b01a6
McCoy Zhu <mz1945@nyu.edu>
2022-04-11 21:23:21 -0400

    ADD: `SemesterSelector`

---
+++ components/SemesterSelector.tsx
@@ -0,0 +1,51 @@
+import React, { useMemo } from "react";
+import { Select, type ISelectProps, Icon } from "native-base";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import Semester from "../libs/semester";
+
+type SemesterOptionRecord = Record<string, Semester>;
+
+type SemesterSelectorBaseProps = {
+  selectedSemester?: Semester;
+  semesterOptions: Semester[];
+  onSelectedSemesterChange: (semester: Semester) => void;
+};
+
+export type SemesterSelectorProps = SemesterSelectorBaseProps &
+  Omit<ISelectProps, keyof SemesterSelectorBaseProps>;
+
+export function SemesterSelector({
+  selectedSemester,
+  semesterOptions,
+  onSelectedSemesterChange,
+  ...rest
+}: SemesterSelectorProps) {
+  const semesterOptionsRecord = useMemo(() => {
+    const record: SemesterOptionRecord = {};
+    for (let semester of semesterOptions) {
+      record[semester.toString()] = semester;
+    }
+    return record;
+  }, [semesterOptions]);
+
+  return (
+    <Select
+      {...rest}
+      selectedValue={selectedSemester?.toString()}
+      onValueChange={(semesterName) => {
+        onSelectedSemesterChange(semesterOptionsRecord[semesterName]);
+      }}
+      _selectedItem={{
+        endIcon: <Icon color={"nyu"} as={<Ionicons name={"checkmark"} />} />,
+      }}
+    >
+      {Object.keys(semesterOptionsRecord).map((semesterName) => (
+        <Select.Item
+          key={semesterName}
+          label={semesterName}
+          value={semesterName}
+        />
+      ))}
+    </Select>
+  );
+}

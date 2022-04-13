6862fee43eca7faed8702bf1a42607173d0b477c
McCoy Zhu <mz1945@nyu.edu>
2022-04-12 21:43:32 -0400

    UPDATE: `ReviewScreen` now has inputs

--- screens/shared/ReviewScreen.tsx
+++ screens/shared/ReviewScreen.tsx
@@ -1,10 +1,78 @@
-import React from "react";
-import { Center } from "native-base";
+import React, { useMemo } from "react";
+import { Box, HStack, Input, Select, Text, VStack } from "native-base";
+import KeyboardAwareSafeAreaScrollView from "../../containers/KeyboardAwareSafeAreaScrollView";
+import SemesterSelector from "../../components/SemesterSelector";
+import Semester from "../../libs/semester";
 
 export default function ReviewScreen() {
+  const semesterOptions = useMemo(
+    () => Semester.getSemesterOptions(true, false).reverse(),
+    []
+  );
+
   return (
-    <Center height={"100%"} backgroundColor={"background.secondary"}>
-      Hello World!
-    </Center>
+    <KeyboardAwareSafeAreaScrollView>
+      <Box marginY={"10px"}>
+        <Text variant={"h1"}>{"Mobile App Development"}</Text>
+        <Text variant={"h2"}>{"DM-UY 4193"}</Text>
+        <VStack marginX={"10px"} marginY={"5px"} space={"8px"}>
+          <Box>
+            <Text variant={"label"} fontWeight={"semibold"} color={"nyu"}>
+              Enjoyment
+            </Text>
+            <Select />
+          </Box>
+          <Box>
+            <Text variant={"label"} fontWeight={"semibold"} color={"nyu"}>
+              Difficulty
+            </Text>
+            <Select />
+          </Box>
+          <Box>
+            <Text variant={"label"} fontWeight={"semibold"} color={"nyu"}>
+              Workload
+            </Text>
+            <Select />
+          </Box>
+          <Box>
+            <Text variant={"label"} fontWeight={"semibold"} color={"nyu"}>
+              Value
+            </Text>
+            <Select />
+          </Box>
+          <Box>
+            <Text variant={"label"} fontWeight={"semibold"} color={"nyu"}>
+              Semester
+            </Text>
+            <SemesterSelector
+              semesterOptions={semesterOptions}
+              onSelectedSemesterChange={() => {}}
+            />
+          </Box>
+          <Box>
+            <Text variant={"label"} fontWeight={"semibold"} color={"nyu"}>
+              Instructor
+            </Text>
+            <Input />
+          </Box>
+          <Box>
+            <HStack>
+              <Text variant={"label"} fontWeight={"semibold"} color={"nyu"}>
+                Comment
+              </Text>
+              <Text
+                variant={"label"}
+                marginLeft={"1px"}
+                fontWeight={"bold"}
+                color={"red.500"}
+              >
+                *
+              </Text>
+            </HStack>
+            <Input placeholder={"Optional"} multiline={true} height={"130px"} />
+          </Box>
+        </VStack>
+      </Box>
+    </KeyboardAwareSafeAreaScrollView>
   );
 }

edfd77f2c36bb10f85114c50adf744e715bd91ad
McCoy Zhu <mz1945@nyu.edu>
2022-04-13 11:24:14 -0400

    UPDATE: `ReviewScreen` actually reading and setting states

--- screens/shared/ReviewScreen.tsx
+++ screens/shared/ReviewScreen.tsx
@@ -1,6 +1,11 @@
-import React, { useMemo } from "react";
+import React, { useState, useMemo, useEffect } from "react";
 import { Box, Input, Text, VStack } from "native-base";
-import { useRoute, type RouteProp } from "@react-navigation/native";
+import {
+  useNavigation,
+  useRoute,
+  type RouteProp,
+} from "@react-navigation/native";
+import { type StackNavigationProp } from "@react-navigation/stack";
 
 import { type SharedNavigationParamList } from "../../libs/types";
 import KeyboardAwareSafeAreaScrollView from "../../containers/KeyboardAwareSafeAreaScrollView";
@@ -9,12 +14,30 @@ import RatingSelector, { RatingType } from "../../components/RatingSelector";
 import SemesterSelector from "../../components/SemesterSelector";
 import Semester from "../../libs/semester";
 import { getFullClassCode } from "../../libs/utils";
+import { Rating } from "../../libs/rating";
+import { useAuth } from "../../mongodb/auth";
+
+type ReviewScreenNavigationProp = StackNavigationProp<
+  SharedNavigationParamList,
+  "Review"
+>;
 
 type ReviewScreenRouteProp = RouteProp<SharedNavigationParamList, "Review">;
 
 export default function ReviewScreen() {
+  const auth = useAuth();
+  const navigation = useNavigation<ReviewScreenNavigationProp>();
   const route = useRoute<ReviewScreenRouteProp>();
   const { classInfo } = route.params;
+
+  const [enjoyment, setEnjoyment] = useState<Rating | undefined>(undefined);
+  const [difficulty, setDifficulty] = useState<Rating | undefined>(undefined);
+  const [workload, setWorkload] = useState<Rating | undefined>(undefined);
+  const [value, setValue] = useState<Rating | undefined>(undefined);
+  const [semester, setSemester] = useState<Semester | undefined>(undefined);
+  const [instructor, setInstructor] = useState("");
+  const [comment, setComment] = useState("");
+
   const semesterOptions = useMemo(
     () => Semester.getSemesterOptions(true, false).reverse(),
     []
@@ -28,39 +51,54 @@ export default function ReviewScreen() {
         <VStack marginX={"10px"} marginY={"5px"} space={"8px"}>
           <LabeledInput label={"Enjoyment"} showRequiredIcon>
             <RatingSelector
+              selectedRating={enjoyment}
               ratingType={RatingType.enjoyment}
-              onSelectedRatingChange={() => {}}
+              onSelectedRatingChange={setEnjoyment}
             />
           </LabeledInput>
           <LabeledInput label={"Difficulty"} showRequiredIcon>
             <RatingSelector
+              selectedRating={difficulty}
               ratingType={RatingType.difficulty}
-              onSelectedRatingChange={() => {}}
+              onSelectedRatingChange={setDifficulty}
             />
           </LabeledInput>
           <LabeledInput label={"Workload"} showRequiredIcon>
             <RatingSelector
+              selectedRating={workload}
               ratingType={RatingType.workload}
-              onSelectedRatingChange={() => {}}
+              onSelectedRatingChange={setWorkload}
             />
           </LabeledInput>
           <LabeledInput label={"Value"} showRequiredIcon>
             <RatingSelector
+              selectedRating={value}
               ratingType={RatingType.value}
-              onSelectedRatingChange={() => {}}
+              onSelectedRatingChange={setValue}
             />
           </LabeledInput>
           <LabeledInput label={"Semester"} showRequiredIcon>
             <SemesterSelector
+              selectedSemester={semester}
               semesterOptions={semesterOptions}
-              onSelectedSemesterChange={() => {}}
+              onSelectedSemesterChange={setSemester}
             />
           </LabeledInput>
           <LabeledInput label={"Instructor"} showRequiredIcon>
-            <Input />
+            <Input
+              value={instructor}
+              onChangeText={setInstructor}
+              autoCapitalize={"words"}
+            />
           </LabeledInput>
           <LabeledInput label={"Comment"}>
-            <Input placeholder={"Optional"} multiline height={"130px"} />
+            <Input
+              placeholder={"Optional"}
+              value={comment}
+              onChangeText={setComment}
+              multiline
+              height={"130px"}
+            />
           </LabeledInput>
         </VStack>
       </Box>

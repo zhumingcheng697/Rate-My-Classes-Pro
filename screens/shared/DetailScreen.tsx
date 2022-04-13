8e3959ccae02adcc41e166faef96dfebaa9a17b1
McCoy Zhu <mz1945@nyu.edu>
2022-04-13 17:55:29 -0400

    ADD: placeholder `ReviewCard` in `DetailScreen`

--- screens/shared/DetailScreen.tsx
+++ screens/shared/DetailScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Text, Button, Box } from "native-base";
+import { Text, Button, Box, VStack, HStack, Spacer } from "native-base";
 import {
   useNavigation,
   useRoute,
@@ -8,10 +8,16 @@ import {
 import { type StackNavigationProp } from "@react-navigation/stack";
 import { useSelector } from "react-redux";
 
-import type { SharedNavigationParamList } from "../../libs/types";
-import { getDepartmentName, getSchoolName } from "../../libs/utils";
+import type { Review, SharedNavigationParamList } from "../../libs/types";
+import {
+  getDepartmentName,
+  getSchoolName,
+  placeholderReview,
+} from "../../libs/utils";
+import Semester from "../../libs/semester";
 import KeyboardAwareSafeAreaScrollView from "../../containers/KeyboardAwareSafeAreaScrollView";
 import { useAuth } from "../../mongodb/auth";
+import PlainTextButton from "../../components/PlainTextButton";
 
 type DetailScreenNavigationProp = StackNavigationProp<
   SharedNavigationParamList,
@@ -19,6 +25,79 @@ type DetailScreenNavigationProp = StackNavigationProp<
 >;
 
 type DetailScreenRouteProp = RouteProp<SharedNavigationParamList, "Detail">;
+
+type ReviewCardProps = { review: Review };
+
+function ReviewCard({ review }: ReviewCardProps) {
+  const {
+    instructor,
+    semester,
+    enjoyment,
+    difficulty,
+    workload,
+    value,
+    upvotes,
+    downvotes,
+    reviewedDate,
+    comment,
+  } = review;
+
+  return (
+    <VStack
+      background={"background.secondary"}
+      borderRadius={10}
+      space={"5px"}
+      padding={"10px"}
+    >
+      <Text fontSize={"lg"} fontWeight={"semibold"}>{`${new Semester(
+        semester
+      ).toString()} with ${instructor}`}</Text>
+      <HStack flexWrap={"wrap"}>
+        <HStack>
+          <Text fontSize={"md"} fontWeight={"medium"}>
+            {"Enjoyment: "}
+          </Text>
+          <Text fontSize={"md"}>{enjoyment} / 5</Text>
+        </HStack>
+        <Box minWidth={"25px"} />
+        <HStack>
+          <Text fontSize={"md"} fontWeight={"medium"}>
+            {"Difficulty: "}
+          </Text>
+          <Text fontSize={"md"}>{difficulty} / 5</Text>
+        </HStack>
+        <Box minWidth={"25px"} />
+        <HStack>
+          <Text fontSize={"md"} fontWeight={"medium"}>
+            {"Workload: "}
+          </Text>
+          <Text fontSize={"md"}>{workload} / 5</Text>
+        </HStack>
+        <Box minWidth={"25px"} />
+        <HStack>
+          <Text fontSize={"md"} fontWeight={"medium"}>
+            {"Value: "}
+          </Text>
+          <Text fontSize={"md"}>{value} / 5</Text>
+        </HStack>
+      </HStack>
+      {comment && <Text fontSize={"md"}>{comment}</Text>}
+      <HStack justifyContent={"space-between"} flexWrap={"wrap"}>
+        <PlainTextButton
+          title={"Edit My Review"}
+          _text={{ fontWeight: "semibold" }}
+        />
+        <Text>
+          {new Date(reviewedDate).toLocaleDateString(undefined, {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          })}
+        </Text>
+      </HStack>
+    </VStack>
+  );
+}
 
 export default function DetailScreen() {
   const navigation = useNavigation<DetailScreenNavigationProp>();
@@ -59,6 +138,11 @@ export default function DetailScreen() {
             {auth.isAuthenticated ? "Review" : "Sign Up to Review"}
           </Text>
         </Button>
+        <VStack margin={"10px"} space={"10px"}>
+          <ReviewCard review={placeholderReview} />
+          <ReviewCard review={placeholderReview} />
+          <ReviewCard review={placeholderReview} />
+        </VStack>
       </Box>
     </KeyboardAwareSafeAreaScrollView>
   );

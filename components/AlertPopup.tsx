2fd647f3d6a9712ee3fdf7eb00ac78c2ebc3adc2
McCoy Zhu <mz1945@nyu.edu>
2022-04-13 21:48:11 -0400

    ADD: `footerPrimaryButton` for `AlertPopup`

--- components/AlertPopup.tsx
+++ components/AlertPopup.tsx
@@ -1,31 +1,29 @@
 import React, {
   useRef,
   type ReactNode,
+  type ReactElement,
   type MutableRefObject,
   useEffect,
 } from "react";
 import { Keyboard } from "react-native";
 import { Button, AlertDialog } from "native-base";
-import { type IAlertDialogProps } from "native-base/lib/typescript/components/composites";
 
-type AlertPopupBaseProps = {
+export type AlertPopupProps = {
   isOpen: boolean;
   header?: string;
   body?: string;
   footer?: (ref: MutableRefObject<any>) => ReactNode;
+  footerPrimaryButton?: ReactElement;
   onClose: () => any;
 };
-
-export type AlertPopupProps = AlertPopupBaseProps &
-  Omit<IAlertDialogProps, keyof AlertPopupBaseProps | "leastDestructiveRef">;
 
 export default function AlertPopup({
   header = "Unable to Load Class Information",
   body = "Please check your network connection or try again later.",
   isOpen,
   footer,
+  footerPrimaryButton,
   onClose,
-  ...rest
 }: AlertPopupProps) {
   const ref = useRef();
 
@@ -36,18 +34,26 @@ export default function AlertPopup({
   }, [isOpen]);
 
   return (
-    <AlertDialog
-      leastDestructiveRef={ref}
-      isOpen={isOpen}
-      onClose={onClose}
-      {...rest}
-    >
+    <AlertDialog leastDestructiveRef={ref} isOpen={isOpen} onClose={onClose}>
       <AlertDialog.Content>
         <AlertDialog.Header>{header}</AlertDialog.Header>
         <AlertDialog.Body>{body}</AlertDialog.Body>
         <AlertDialog.Footer>
           {footer ? (
             footer(ref)
+          ) : footerPrimaryButton ? (
+            <Button.Group space={2}>
+              <Button
+                variant="unstyled"
+                _pressed={{ opacity: 0.5 }}
+                _hover={{ opacity: 0.5 }}
+                onPress={onClose}
+                ref={ref}
+              >
+                Cancel
+              </Button>
+              {footerPrimaryButton}
+            </Button.Group>
           ) : (
             <Button ref={ref} onPress={onClose}>
               OK

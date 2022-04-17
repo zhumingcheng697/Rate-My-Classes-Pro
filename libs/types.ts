812131eac7aa621c30b6cef0c8224dc4b2f2da6c
McCoy Zhu <mz1945@nyu.edu>
2022-04-16 22:28:41 -0400

    FIX: `scheduleScreenOptions`

--- libs/types.ts
+++ libs/types.ts
@@ -86,7 +86,7 @@ export type RootNavigationParamList = {
 export type SharedNavigationParamList = {
   Detail: { classInfo: ClassInfo; deleteReview?: true; newReview?: Review };
   Review: { classInfo: ClassInfo; previousReview?: Review; newReview?: Review };
-  Schedule: SectionInfo[];
+  Schedule: { semester: SemesterInfo; sections: SectionInfo[] };
   SignInSignUp: { isSigningIn: boolean } | undefined;
 };
 
--- navigation/options/scheduleScreenOptions.ts
+++ navigation/options/scheduleScreenOptions.ts
@@ -1,12 +1,27 @@
-import { useSelector } from "react-redux";
-import { type StackNavigationOptions } from "@react-navigation/stack";
+import { type RouteProp } from "@react-navigation/native";
+import type {
+  StackNavigationProp,
+  StackNavigationOptions,
+} from "@react-navigation/stack";
 
 import Semester from "../../libs/semester";
+import { type SharedNavigationParamList } from "../../libs/types";
 
-export default (): StackNavigationOptions => {
-  const { selectedSemester } = useSelector((state) => state.settings);
-  return {
-    headerBackTitle: "Back",
-    title: `${new Semester(selectedSemester).toString()} Schedule`,
-  };
+type ScheduleScreenNavigationProp = StackNavigationProp<
+  SharedNavigationParamList,
+  "Schedule"
+>;
+
+type ScheduleScreenRouteProp = RouteProp<SharedNavigationParamList, "Schedule">;
+
+export type ScheduleScreenOptionsProp = {
+  navigation: ScheduleScreenNavigationProp;
+  route: ScheduleScreenRouteProp;
 };
+
+export default ({
+  route,
+}: ScheduleScreenOptionsProp): StackNavigationOptions => ({
+  headerBackTitle: "Back",
+  title: `${new Semester(route.params.semester).toString()} Schedule`,
+});

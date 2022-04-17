3c644009365eda7062162c118144897de6c88088
McCoy Zhu <mz1945@nyu.edu>
2022-04-16 22:33:10 -0400

    UPDATE: `scheduleScreenOptions`

--- navigation/options/scheduleScreenOptions.ts
+++ navigation/options/scheduleScreenOptions.ts
@@ -1,27 +1,17 @@
 import { type RouteProp } from "@react-navigation/native";
-import type {
-  StackNavigationProp,
-  StackNavigationOptions,
-} from "@react-navigation/stack";
+import type { StackNavigationOptions } from "@react-navigation/stack";
 
 import Semester from "../../libs/semester";
 import { type SharedNavigationParamList } from "../../libs/types";
 
-type ScheduleScreenNavigationProp = StackNavigationProp<
-  SharedNavigationParamList,
-  "Schedule"
->;
-
 type ScheduleScreenRouteProp = RouteProp<SharedNavigationParamList, "Schedule">;
 
 export type ScheduleScreenOptionsProp = {
-  navigation: ScheduleScreenNavigationProp;
   route: ScheduleScreenRouteProp;
 };
 
 export default ({
   route,
 }: ScheduleScreenOptionsProp): StackNavigationOptions => ({
-  headerBackTitle: "Back",
   title: `${new Semester(route.params.semester).toString()} Schedule`,
 });

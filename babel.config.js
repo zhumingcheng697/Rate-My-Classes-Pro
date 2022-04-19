25c561e7361a7eab98f019c3270d06f2e5dfe18d
McCoy Zhu <mz1945@nyu.edu>
2022-04-19 13:49:53 -0400

    UPDATE: check for `process` in `babel.config.js`

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,28 @@
-module.exports = {
-  presets: ["module:metro-react-native-babel-preset"],
-  plugins: [["transform-inline-environment-variables"]],
+const isUsingExpoWithNPM =
+  process.env &&
+  process.env._ &&
+  (process.env._.endsWith("/expo") || process.env._.endsWith("/expo-cli"));
+
+const isUsingExpoWithCLI =
+  process.argv &&
+  process.argv[1] &&
+  (process.argv[1].endsWith("/expo") || process.argv[1].endsWith("/expo-cli"));
+
+const reactNativeConfig = function (api) {
+  api.cache(true);
+  return {
+    presets: ["module:metro-react-native-babel-preset"],
+    plugins: [["transform-inline-environment-variables"]],
+  };
 };
 
-// module.exports = function (api) {
-//   api.cache(true);
-//   return {
-//     presets: ["babel-preset-expo"],
-//     plugins: [["transform-inline-environment-variables"]],
-//   };
-// };
+const expoConfig = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [["transform-inline-environment-variables"]],
+  };
+};
+
+module.exports =
+  isUsingExpoWithNPM || isUsingExpoWithCLI ? expoConfig : reactNativeConfig;

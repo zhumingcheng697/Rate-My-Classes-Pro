41f30d7e9ae382081ee977ace641e32013493617
McCoy Zhu <mz1945@nyu.edu>
2022-04-18 23:34:55 -0400

    UPDATE: having babel config for both expo and non-expo

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,12 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ["babel-preset-expo"],
-    plugins: [["transform-inline-environment-variables"]],
-  };
+module.exports = {
+  presets: ["module:metro-react-native-babel-preset"],
+  plugins: [["transform-inline-environment-variables"]],
 };
+
+// module.exports = function (api) {
+//   api.cache(true);
+//   return {
+//     presets: ["babel-preset-expo"],
+//     plugins: [["transform-inline-environment-variables"]],
+//   };
+// };

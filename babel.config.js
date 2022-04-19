3114c4740aa20583dd596bf99cd0e9c56cf1dfac
McCoy Zhu <mz1945@nyu.edu>
2022-04-18 20:32:41 -0400

    UPDATE: adding `expo` back for `react-native-web`

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,7 @@
-module.exports = {
-  presets: ["module:metro-react-native-babel-preset"],
-  plugins: [["transform-inline-environment-variables"]],
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ["babel-preset-expo"],
+    plugins: [["transform-inline-environment-variables"]],
+  };
 };

fb649616fa8ed74190698ba6cd437a77d7bfc4a8
McCoy Zhu <mz1945@nyu.edu>
2022-09-29 00:06:03 -0400

    ADD: `colors.ts` for main colors

---
+++ libs/colors.ts
@@ -0,0 +1,16 @@
+const colors = {
+  background: {
+    primary: { light: "#ffffff", dark: "#000000" },
+    secondary: { light: "#f2f2f7", dark: "#1c1c1e" },
+    secondaryWithOpacity: { light: "#f2f2f777", dark: "#1c1c1e77" },
+    tertiary: { light: "#e5e5ea", dark: "#2c2c2e" },
+    tertiaryWithOpacity: { light: "#e5e5ea77", dark: "#2c2c2e77" },
+  },
+  nyu: { light: "#57068c", dark: "#c05eff" },
+};
+
+export default colors;
+
+export type Colors = typeof colors;
+
+export type ColorPair = { light: string; dark: string };

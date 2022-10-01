0133258fd6b2e2b70d8fe93a917c062aaa57d532
McCoy Zhu <mz1945@nyu.edu>
2022-09-30 20:50:53 -0400

    FIX: color with opacity in dark mode hard to see

--- libs/colors.ts
+++ libs/colors.ts
@@ -2,9 +2,9 @@ const colors = {
   background: {
     primary: { light: "#ffffff", dark: "#000000" },
     secondary: { light: "#f2f2f7", dark: "#1c1c1e" },
-    secondaryWithOpacity: { light: "#f2f2f777", dark: "#1c1c1e77" },
+    secondaryWithOpacity: { light: "#f2f2f777", dark: "#1c1c1eaa" },
     tertiary: { light: "#e5e5ea", dark: "#2c2c2e" },
-    tertiaryWithOpacity: { light: "#e5e5ea77", dark: "#2c2c2e77" },
+    tertiaryWithOpacity: { light: "#e5e5ea77", dark: "#2c2c2eaa" },
   },
   nyu: { light: "#57068c", dark: "#c05eff" },
 };

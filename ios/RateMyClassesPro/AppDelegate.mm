4b02246e8d100b3ea8c508301370e3d7b9a617f4
McCoy Zhu <mz1945@nyu.edu>
2022-10-01 21:48:30 -0400

    REMOVE: extra menu bar item for mac

--- ios/RateMyClassesPro/AppDelegate.mm
+++ ios/RateMyClassesPro/AppDelegate.mm
@@ -27,6 +27,18 @@
 
 @implementation AppDelegate
 
+- (void)buildMenuWithBuilder:(id<UIMenuBuilder>)builder
+{
+  if (@available(iOS 15.0, macCatalyst 15.0, *)) {
+    [builder removeMenuForIdentifier:UIMenuSidebar];
+  }
+  [builder removeMenuForIdentifier:UIMenuFile];
+  [builder removeMenuForIdentifier:UIMenuEdit];
+  [builder removeMenuForIdentifier:UIMenuFormat];
+  [builder removeMenuForIdentifier:UIMenuServices];
+  [builder removeMenuForIdentifier:UIMenuToolbar];
+}
+
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
   RCTAppSetupPrepareApp(application);

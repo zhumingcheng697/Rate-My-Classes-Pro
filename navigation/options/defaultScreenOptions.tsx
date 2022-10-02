55d53e5c749cb88df6d8b24bd1b2174b684b8264
McCoy Zhu <mz1945@nyu.edu>
2022-10-02 02:15:22 -0400

    UPDATE: set header border color with `useTheme`

--- navigation/options/defaultScreenOptions.tsx
+++ navigation/options/defaultScreenOptions.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { Platform, useColorScheme } from "react-native";
 import { Icon } from "native-base";
 import {
   TransitionPresets,
   type StackNavigationOptions,
 } from "@react-navigation/stack";
-import { DarkTheme, DefaultTheme } from "@react-navigation/native";
+import { useTheme } from "@react-navigation/native";
 import Ionicons from "react-native-vector-icons/Ionicons";
 
 import colors from "../../libs/colors";
@@ -16,19 +15,7 @@ export default (): StackNavigationOptions => ({
   headerStyle: {
     shadowColor: "transparent",
     borderBottomWidth: 1,
-    ...(Platform.OS === "ios" || Platform.OS === "macos"
-      ? colorModeResponsiveStyle((selector) => ({
-          borderColor: selector({
-            light: DefaultTheme.colors.border,
-            dark: DarkTheme.colors.border,
-          }),
-        }))
-      : {
-          borderColor:
-            useColorScheme() === "dark"
-              ? DarkTheme.colors.border
-              : DefaultTheme.colors.border,
-        }),
+    borderColor: useTheme().colors.border,
   },
   headerBackImage: () => (
     <Icon

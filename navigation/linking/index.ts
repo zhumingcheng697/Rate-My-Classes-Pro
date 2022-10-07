653d85888340c49bb4df8d29363020d88e0012ba
McCoy Zhu <mz1945@nyu.edu>
2022-10-07 00:27:48 -0400

    UPDATE: using `stringifyRoute`

--- navigation/linking/index.ts
+++ navigation/linking/index.ts
@@ -4,91 +4,13 @@ import {
   LinkingOptions,
 } from "@react-navigation/native";
 
+import stringnifyRoute from "./stringify";
 import {
   RootNavigationParamList,
   ExploreNavigationParamList,
   SearchNavigationParamList,
   MeNavigationParamList,
-  ClassInfo,
-  StarredOrReviewed,
 } from "../../libs/types";
-
-type RouteToPathMap<ParamList> = {
-  [Screen in keyof ParamList]: (param: ParamList[Screen]) => string;
-};
-
-const getPathForClass = (classInfo: ClassInfo) =>
-  `${classInfo.schoolCode}/${classInfo.departmentCode}/${classInfo.classNumber}`;
-
-const addQueryParam = (query: string | undefined) =>
-  query ? `?query=${encodeURIComponent(query)}` : "";
-
-const checkStarredOrReviewed = (
-  starredOrReviewed: StarredOrReviewed | undefined
-) => starredOrReviewed?.toLowerCase() || "starred";
-
-const exploreRouteToPath: RouteToPathMap<ExploreNavigationParamList> = {
-  University: () => "explore",
-  School: ({ schoolCode }) => `explore/${schoolCode}`,
-  Department: ({ schoolCode, departmentCode }) =>
-    `explore/${schoolCode}/${departmentCode}`,
-  Detail: ({ classInfo }) => `explore/${getPathForClass(classInfo)}`,
-  Review: ({ classInfo }) => `explore/${getPathForClass(classInfo)}/review`,
-  Schedule: ({ classInfo }) => `explore/${getPathForClass(classInfo)}/schedule`,
-  SignInSignUp: ({ classInfo, isSigningUp }) =>
-    classInfo
-      ? `explore/${getPathForClass(classInfo)}/${
-          isSigningUp ? "sign-up" : "sign-in"
-        }`
-      : isSigningUp
-      ? "sign-up"
-      : "sign-in",
-};
-
-const searchRouteToPath: RouteToPathMap<SearchNavigationParamList> = {
-  Search: ({ query }) => "search" + addQueryParam(query),
-  Detail: ({ classInfo, query }) =>
-    `search/${getPathForClass(classInfo)}` + addQueryParam(query),
-  Review: ({ classInfo, query }) =>
-    `search/${getPathForClass(classInfo)}/review` + addQueryParam(query),
-  Schedule: ({ classInfo, query }) =>
-    `search/${getPathForClass(classInfo)}/schedule` + addQueryParam(query),
-  SignInSignUp: ({ classInfo, isSigningUp, query }) =>
-    classInfo
-      ? `search/${getPathForClass(classInfo)}/${
-          isSigningUp ? "sign-up" : "sign-in"
-        }` + addQueryParam(query)
-      : isSigningUp
-      ? "sign-up"
-      : "sign-in",
-};
-
-const meRouteToPath: RouteToPathMap<MeNavigationParamList> = {
-  Account: () => "account",
-  Starred: () => "starred",
-  Reviewed: () => "reviewed",
-  Settings: () => "settings",
-  Detail: ({ classInfo, starredOrReviewed }) =>
-    `${checkStarredOrReviewed(starredOrReviewed)}/${getPathForClass(
-      classInfo
-    )}`,
-  Review: ({ classInfo, starredOrReviewed }) =>
-    `${checkStarredOrReviewed(starredOrReviewed)}/${getPathForClass(
-      classInfo
-    )}/review`,
-  Schedule: ({ classInfo, starredOrReviewed }) =>
-    `${checkStarredOrReviewed(starredOrReviewed)}/${getPathForClass(
-      classInfo
-    )}/schedule`,
-  SignInSignUp: ({ classInfo, isSigningUp, starredOrReviewed }) =>
-    classInfo
-      ? `${checkStarredOrReviewed(starredOrReviewed)}/${getPathForClass(
-          classInfo
-        )}/${isSigningUp ? "sign-up" : "sign-in"}`
-      : isSigningUp
-      ? "sign-up"
-      : "sign-in",
-};
 
 const linking: LinkingOptions<RootNavigationParamList> = {
   prefixes: [
@@ -112,24 +34,17 @@ const linking: LinkingOptions<RootNavigationParamList> = {
     //       const { name: screenName, params: screenParams } =
     //         stackRoutes[stackIndex] ?? {};
 
+    //       type Nav =
+    //         | ExploreNavigationParamList
+    //         | SearchNavigationParamList
+    //         | MeNavigationParamList;
+
     //       if (screenName) {
-    //         switch (tabName as keyof RootNavigationParamList) {
-    //           case "ExploreTab":
-    //             const exploreMapper = exploreRouteToPath[
-    //               screenName as keyof ExploreNavigationParamList
-    //             ] as (params: typeof screenParams) => string;
-    //             return exploreMapper(screenParams);
-    //           case "SearchTab":
-    //             const searchMapper = searchRouteToPath[
-    //               screenName as keyof SearchNavigationParamList
-    //             ] as (params: typeof screenParams) => string;
-    //             return searchMapper(screenParams);
-    //           case "MeTab":
-    //             const meMapper = meRouteToPath[
-    //               screenName as keyof MeNavigationParamList
-    //             ] as (params: typeof screenParams) => string;
-    //             return meMapper(screenParams);
-    //         }
+    //         return stringnifyRoute(
+    //           tabName as keyof RootNavigationParamList,
+    //           screenName as keyof Nav,
+    //           screenParams as Nav[keyof Nav]
+    //         );
     //       }
     //     }
     //   }

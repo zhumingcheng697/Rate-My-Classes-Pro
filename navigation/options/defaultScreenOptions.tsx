710927daba54dc5b0cc6fa4627d74a7bafb637bf
McCoy Zhu <mz1945@nyu.edu>
2022-10-14 20:31:49 -0400

    UPDATE: back button supports linking on web

--- navigation/options/defaultScreenOptions.tsx
+++ navigation/options/defaultScreenOptions.tsx
@@ -1,13 +1,49 @@
 import React from "react";
-import { Icon } from "native-base";
+import { Platform } from "react-native";
+import { CommonActions } from "@react-navigation/native";
+import { type HeaderBackButtonProps } from "@react-navigation/elements";
+import { Icon, type IIconProps } from "native-base";
 import {
   TransitionPresets,
   type StackNavigationOptions,
 } from "@react-navigation/stack";
 import Ionicons from "react-native-vector-icons/Ionicons";
 
+import { Pressable } from "../../components/LinkCompatibleButtons";
 import colors, { subtleBorder } from "../../styling/colors";
 import { colorModeResponsiveStyle } from "../../styling/color-mode-utils";
+import { useInitialPreviousRoute, useInitialTabName } from "../../libs/hooks";
+import { Route } from "../../libs/utils";
+
+function ChevronIcon(props: IIconProps) {
+  return (
+    <Icon
+      {...props}
+      size={"md"}
+      {...colorModeResponsiveStyle((selector) => ({
+        color: selector(colors.nyu),
+      }))}
+      as={<Ionicons name={"chevron-back"} />}
+    />
+  );
+}
+
+function WebBackButton(props: HeaderBackButtonProps) {
+  const { onPress: _, canGoBack, ...rest } = props;
+  const tabName = useInitialTabName();
+  const previousRoute = useInitialPreviousRoute();
+  return !canGoBack ? null : (
+    <Pressable
+      {...rest}
+      linkTo={{
+        ...Route(tabName, previousRoute?.name, previousRoute?.params),
+        action: CommonActions.goBack(),
+      }}
+    >
+      <ChevronIcon marginLeft={"11px"} marginRight={"8px"} />
+    </Pressable>
+  );
+}
 
 export default (): StackNavigationOptions => ({
   ...TransitionPresets.DefaultTransition,
@@ -16,14 +52,10 @@ export default (): StackNavigationOptions => ({
     borderBottomWidth: 1,
     borderBottomColor: subtleBorder,
   },
-  headerBackImage: () => (
-    <Icon
-      size={"md"}
-      marginRight={"-3px"}
-      {...colorModeResponsiveStyle((selector) => ({
-        color: selector(colors.nyu),
-      }))}
-      as={<Ionicons name={"chevron-back"} />}
-    />
-  ),
+  headerLeft:
+    Platform.OS === "web" ? (props) => <WebBackButton {...props} /> : undefined,
+  headerBackImage:
+    Platform.OS === "web"
+      ? undefined
+      : () => <ChevronIcon marginRight={"-3px"} />,
 });

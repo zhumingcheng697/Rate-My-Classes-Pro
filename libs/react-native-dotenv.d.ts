c42d375a1954dd30db1482562e71cd6ffe005659
McCoy Zhu <mz1945@nyu.edu>
2022-10-16 12:57:51 -0400

    ADD: `WEB_DEPLOYMENT_URL` in `.env`

--- libs/react-native-dotenv.d.ts
+++ libs/react-native-dotenv.d.ts
@@ -2,4 +2,5 @@ declare module "react-native-dotenv" {
   export const REALM_APP_ID: string;
   export const GOOGLE_WEB_CLIENT_ID: string;
   export const GOOGLE_IOS_CLIENT_ID: string;
+  export const WEB_DEPLOYMENT_URL: string;
 }

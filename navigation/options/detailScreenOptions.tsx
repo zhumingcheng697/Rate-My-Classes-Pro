ada397fb7117f1b29390ea341dab2b187489fa2e
McCoy Zhu <mz1945@nyu.edu>
2022-10-16 18:33:02 -0400

    FIX: not passing `classInfo` when should have

--- navigation/options/detailScreenOptions.tsx
+++ navigation/options/detailScreenOptions.tsx
@@ -80,7 +80,9 @@ export default ({
               <Button
                 onPress={() => {
                   setShowAlert(false);
-                  navigation.navigate("SignInSignUp", { classCode });
+                  navigation.navigate("SignInSignUp", {
+                    classCode: classInfo ?? classCode,
+                  });
                 }}
               >
                 Sign In

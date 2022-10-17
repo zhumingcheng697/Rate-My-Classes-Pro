eeb1d70894ced63ccbec989abe7818b9956b9717
McCoy Zhu <mz1945@nyu.edu>
2022-10-17 12:44:13 -0400

    FIX: `ClearableInput` clear on web with `onMouseUp`

--- components/ClearableInput.tsx
+++ components/ClearableInput.tsx
@@ -56,6 +56,12 @@ export default function ClearableInput({
               onPress={() => {
                 onChangeText("");
               }}
+              _web={{
+                // @ts-ignore
+                onMouseUp: () => {
+                  onChangeText("");
+                },
+              }}
             />
           )}
         </Box>

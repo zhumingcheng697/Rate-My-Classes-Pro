ee0a4d217387a863b198aca524853640e0dcf794
McCoy Zhu <mz1945@nyu.edu>
2022-10-20 21:26:10 -0400

    ADD: experimental sync on native

---
+++ mongodb/sync.ts
@@ -0,0 +1,108 @@
+import Realm from "./Realm";
+
+import type { UserDoc } from "./types";
+
+enum Schema {
+  user = "user",
+  starred = "user_starred",
+  reviewed = "user_reviewed",
+  settings = "user_settings",
+  selectedSemester = "user_settings_selectedSemester",
+}
+
+export default function sync(
+  user: Realm.User,
+  callback: (userDoc: Partial<UserDoc>) => void
+) {
+  const userSchema: Realm.ObjectSchema = {
+    name: Schema.user,
+    properties: {
+      _id: "string",
+      username: "string",
+      starred: Schema.starred + "[]",
+      reviewed: Schema.reviewed + "[]",
+      settings: Schema.settings,
+    },
+    primaryKey: "_id",
+  };
+
+  const starredSchema: Realm.ObjectSchema = {
+    name: Schema.starred,
+    embedded: true,
+    properties: {
+      classNumber: "string?",
+      departmentCode: "string?",
+      description: "string?",
+      name: "string?",
+      schoolCode: "string?",
+      starredDate: "int?",
+    },
+  };
+
+  const reviewedSchema: Realm.ObjectSchema = {
+    name: Schema.reviewed,
+    embedded: true,
+    properties: {
+      classNumber: "string?",
+      departmentCode: "string?",
+      description: "string?",
+      name: "string?",
+      reviewedDate: "int?",
+      schoolCode: "string?",
+    },
+  };
+
+  const settingsSchema: Realm.ObjectSchema = {
+    name: Schema.settings,
+    embedded: true,
+    properties: {
+      selectedSemester: Schema.selectedSemester,
+      showPreviousSemesters: "bool?",
+    },
+  };
+
+  const selectedSemesterSchema: Realm.ObjectSchema = {
+    name: Schema.selectedSemester,
+    embedded: true,
+    properties: {
+      semesterCode: "string?",
+      year: "int?",
+    },
+  };
+
+  try {
+    const realm = new Realm({
+      schema: [
+        userSchema,
+        starredSchema,
+        reviewedSchema,
+        settingsSchema,
+        selectedSemesterSchema,
+      ],
+      sync: { user, flexible: true },
+    });
+
+    const userObj = realm
+      .objects<UserDoc>(Schema.user)
+      .filtered(`_id == "${user.id}"`);
+
+    realm.subscriptions.update((sub) => sub.add(userObj));
+
+    userObj.addListener((user) => {
+      const userDoc = user.isValid() && user[0]?.toJSON();
+      if (userDoc) {
+        callback(userDoc);
+      }
+    });
+
+    return () => {
+      try {
+        realm.close();
+      } catch (e) {
+        console.error(e);
+      }
+    };
+  } catch (e) {
+    console.error(e);
+  }
+}

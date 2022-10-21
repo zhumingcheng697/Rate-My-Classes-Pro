62c357256570a3c88018711a6355bd5957c9557e
McCoy Zhu <mz1945@nyu.edu>
2022-10-21 00:13:05 -0400

    UPDATE: refresh netinfo on active in `App`

--- App.tsx
+++ App.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { NativeBaseProvider } from "native-base";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import NetInfo from "@react-native-community/netinfo";
 import Ionicons from "react-native-vector-icons/Ionicons";
 
 import linking from "./navigation/linking";
@@ -11,6 +12,7 @@ import { AuthProvider } from "./mongodb/auth";
 import reducer from "./redux/reducers";
 import { useColorModeSynchronizer } from "./styling/color-mode-utils";
 import nativeBaseTheme from "./styling/theme";
+import { useAppState } from "./libs/hooks";
 import "./libs/GoogleSignIn";
 
 Ionicons.loadFont();
@@ -31,6 +33,11 @@ declare module "react-redux" {
 
 export default function App() {
   const { colorModeManager, navigationTheme } = useColorModeSynchronizer();
+  const appState = useAppState();
+
+  useEffect(() => {
+    if (appState === "active") NetInfo.refresh();
+  }, [appState]);
 
   return (
     <Provider store={store}>

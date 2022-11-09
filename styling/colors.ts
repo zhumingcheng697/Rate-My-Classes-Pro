4bfc57402f1e976fccbe3aeb1085319d0d9910a8
McCoy Zhu <mz1945@nyu.edu>
2022-11-09 14:31:11 -0500

    UPDATE: bump up contrast

--- styling/colors.ts
+++ styling/colors.ts
@@ -1,8 +1,8 @@
 const colors = {
   background: {
     primary: { light: "#ffffff", dark: "#000000" },
-    secondary: { light: "#f2f2f7", dark: "#1c1c1e" },
-    secondaryWithOpacity: { light: "#f2f2f777", dark: "#1c1c1eaa" },
+    secondary: { light: "#f0f0f5", dark: "#202022" },
+    secondaryWithOpacity: { light: "#f0f0f577", dark: "#202022" },
     tertiary: { light: "#e5e5ea", dark: "#2c2c2e" },
     tertiaryWithOpacity: { light: "#e5e5ea77", dark: "#2c2c2eaa" },
   },

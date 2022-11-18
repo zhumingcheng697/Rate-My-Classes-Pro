63e1ad7f514b03bfd4dab9d9f183fe60bd842ed1
McCoy Zhu <mz1945@nyu.edu>
2022-11-18 00:27:11 -0500

    ADD: check email regex in server code, better styling

--- backend/send-code.js
+++ backend/send-code.js
@@ -2,6 +2,9 @@ exports = async function ({ query }) {
   if (!query || !query.email || !query.id)
     throw new Error("Invalid query parameters");
 
+  if (!/^(?:[^\s.][^\s]*[^\s.]|[^\s.]+)+@nyu\.edu$/i.test(query.email))
+    throw new Error("Invalid email address. Please use your nyu.edu email.");
+
   const crypto = require("crypto");
   const code = crypto.randomBytes(3).toString("hex");
 
@@ -29,7 +32,7 @@ exports = async function ({ query }) {
     from: `Rate My Classes Pro <${context.values.get("email-address")}>`,
     to: query.email,
     subject: "Rate My Classes Confirmation Code",
-    html: `<div><p>Thank you for verifying your Rate My Classes account!</p><p>Your confirmation code is: </p><p style="font-size: 2.5em; font-weight:700; margin: 1rem 0; font-family: monospace;">${code}</p><p>This code will expire in 30 minutes.</p></div>`,
+    html: `<div><p>Thank you for verifying your Rate My Classes account.</p><p>Your confirmation code is:</p><p style="font-size: 2.5em; font-weight: 700; margin: 1rem 0; color: #57068c; font-family: ui-monospace, 'SF Mono', SFMono-Regular, 'DejaVu Sans Mono', Menlo, Consolas, monospace;">${code}</p><p>This code will expire in 30 minutes.</p></div>`,
   });
 
   return query.id;

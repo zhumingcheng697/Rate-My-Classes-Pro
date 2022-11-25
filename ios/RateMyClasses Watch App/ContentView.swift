a68b6d331e01c30e0563efbd765e485b360f86ba
McCoy Zhu <mz1945@nyu.edu>
2022-11-25 11:47:12 -0500

    ADD: watch app setup

---
+++ ios/RateMyClasses Watch App/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  RateMyClasses Watch App
+//
+//  Created by McCoy Zhu on 11/24/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundColor(.accentColor)
+      
+      Text("Hello, World!")
+    }
+    .padding()
+  }
+}
+
+struct ContentView_Previews: PreviewProvider {
+  static var previews: some View {
+    ContentView()
+  }
+}
---
+++ ios/RateMyClasses Watch App/RateMyClassesApp.swift
@@ -0,0 +1,17 @@
+//
+//  RateMyClassesApp.swift
+//  RateMyClasses Watch App
+//
+//  Created by McCoy Zhu on 11/24/22.
+//
+
+import SwiftUI
+
+@main
+struct RateMyClassesWatchApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}

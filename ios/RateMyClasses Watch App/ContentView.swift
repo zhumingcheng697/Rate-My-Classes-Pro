3d32b9bce5e9c629d6b207534e909927414c7655
McCoy Zhu <mz1945@nyu.edu>
2022-11-26 11:01:55 -0500

    UDPATE: testing watch UI

--- ios/RateMyClasses Watch App/ContentView.swift
+++ ios/RateMyClasses Watch App/ContentView.swift
@@ -8,15 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+  @State var semesters: Array<String> = [];
+  
   var body: some View {
     VStack {
-      Image(systemName: "globe")
+      Image(systemName: "graduationcap.fill")
         .imageScale(.large)
         .foregroundColor(.accentColor)
       
-      Text("Hello, World!")
+      Text("Rate My Classes")
+      
+      if self.semesters.count == 0 {
+        Button("Load Data") {
+          guard let url = URL(string: "https://nyu.a1liu.com/api/terms") else { return }
+          URLSession.shared.dataTask(with: URLRequest(url: url)) { data, res, err in
+            guard let data = data else { return }
+            if let list = try? JSONDecoder().decode(Array<String>.self, from: data) {
+              self.semesters = list;
+            }
+          }.resume()
+        }.buttonStyle(.bordered)
+      } else {
+        ScrollView {
+          ForEach(self.semesters, id: \.self) { semester in
+            Text(semester) 
+          }
+          
+          Button("Clear") {
+            self.semesters = [];
+          }.buttonBorderShape(.capsule).padding(.vertical)
+        }
+      }
     }
     .padding()
+    .ignoresSafeArea(.container, edges: self.semesters.count == 0 ? [] : [.bottom])
   }
 }
 

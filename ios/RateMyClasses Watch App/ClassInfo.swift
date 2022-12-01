69d58fece75a214f7d3c275c89394a9cdfcad1ca
McCoy Zhu <mz1945@nyu.edu>
2022-11-30 21:35:58 -0500

    ADD: schedule stringify

--- ios/RateMyClasses Watch App/ClassInfo.swift
+++ ios/RateMyClasses Watch App/ClassInfo.swift
@@ -5,6 +5,8 @@
 //  Created by McCoy Zhu on 11/30/22.
 //
 
+import Foundation
+
 protocol ClassInfo {
   var schoolCode: String { get }
   var departmentCode: String { get }
@@ -57,4 +59,75 @@ struct SectionInfo: Codable, Hashable {
   let notes: String?
   let prerequisites: String?
   let recitations: [SectionInfo]?
+  
+  var schedule: [String]? {
+    func getTimeIntervalString(from: Date, to: Date) -> String {
+      let dateIntervalFormatter = DateIntervalFormatter()
+      dateIntervalFormatter.calendar = Calendar(identifier: .gregorian)
+      dateIntervalFormatter.dateTemplate = "h:mm"
+      return dateIntervalFormatter.string(from: from, to: to)
+    }
+    
+    func getWeekDayString(date: Date) -> String {
+      let dateFormatter = DateFormatter()
+      dateFormatter.calendar = Calendar(identifier: .gregorian)
+      dateFormatter.dateFormat = "E"
+      return dateFormatter.string(from: date)
+    }
+    
+    if let meetings = meetings {
+      var weeklySchedule = [Int: [(Date, Date)]]()
+      
+      for meeting in meetings {
+        if let begin = try? Date(meeting.beginDate, strategy: .iso8601) {
+          let end = begin + TimeInterval(meeting.minutesDuration * 60)
+          let dayOfWeek = Calendar(identifier: .gregorian).component(.weekday, from: begin) - 1
+          weeklySchedule[dayOfWeek, default: []].append((begin, end))
+        }
+      }
+      
+      var stringifiedSchedule = [Int: String]()
+      
+      for (day, dailySchedule) in weeklySchedule {
+        var stringifiedDailySchedule = [String]()
+        
+        dailySchedule.sorted { (a, b) in
+          a.0 < b.0
+        }.forEach { meeting in
+          let meetingStr = getTimeIntervalString(from: meeting.0, to: meeting.1)
+          if !stringifiedDailySchedule.contains(meetingStr) {
+            stringifiedDailySchedule.append(meetingStr)
+          }
+        }
+        
+        stringifiedSchedule[day] = stringifiedDailySchedule.joined(separator: ", ")
+      }
+      
+      var finalSchedule = [String]()
+      
+      for i in 1...7 {
+        let day = i % 7
+        if let currDaySchedule = weeklySchedule[day], currDaySchedule.count > 0,
+           let currMeeting = stringifiedSchedule[day] {
+          var currDay = [getWeekDayString(date: currDaySchedule[0].0)]
+          weeklySchedule.removeValue(forKey: day)
+          
+          for j in i + 1...7 {
+            let otherDay = j % 7
+            if let otherDaySchedule = weeklySchedule[otherDay],
+               otherDaySchedule.count > 0 && stringifiedSchedule[otherDay] == currMeeting {
+              currDay.append(getWeekDayString(date: otherDaySchedule[0].0))
+              weeklySchedule.removeValue(forKey: otherDay)
+            }
+          }
+          
+          finalSchedule.append("\(currDay.joined(separator: ", ")): \(currMeeting)")
+        }
+      }
+      
+      return finalSchedule
+    }
+    
+    return nil
+  }
 }

5bdd65567f74721a2812953f244f07f92e012fbd
McCoy Zhu <mz1945@nyu.edu>
2022-12-05 20:37:18 -0500

    ADD: `Rating`

---
+++ ios/RateMyClasses Watch App/Rating.swift
@@ -0,0 +1,13 @@
+//
+//  Rating.swift
+//  RateMyClasses Watch App
+//
+//  Created by McCoy Zhu on 12/1/22.
+//
+
+struct Rating: Codable {
+  var enjoyment: Double
+  var difficulty: Double
+  var workload: Double
+  var value: Double
+}
